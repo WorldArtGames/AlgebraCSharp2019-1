e31d9b5a9cce4d75a93ba4c6d8cc236ea24953a8
WorldArtGames <wag.head.office@gmail.com>
2019-02-22 17:41:23 +0100

    dodano dijeljenje i umnožak

---
+++ ConsoleApp1/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cons(
+
+Ispis)
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine(1 + "\n" + 2 + "\n" + 3);
+        Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.Write("Unesite 1. broj: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Unesite 2. broj: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zbroj: {0}", a + b);
+            Console.WriteLine("Razlika: {0}", a - b);
+            Console.WriteLine("Pritisnite \"Enter\" za kraj...");
+            Console.ReadKey();
+
+        }
+       
+
+            
+
+
+    }
+}
