38304061f5df3a8e7569e5e437b5bbd1df8c5f55
WorldArtGames <wag.head.office@gmail.com>
2019-02-22 17:58:51 +0100

    ovo su moje promjene

--- ConsoleApp1/ConsoleApp3/Program.cs
+++ ConsoleApp1/ConsoleApp3/Program.cs
@@ -4,18 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cons(
-
-Ispis)
+namespace Conszbroj
 {
     class Program
     {
         static void Main(string[] args)
         {
-        Console.WriteLine(1 + "\n" + 2 + "\n" + 3);
-        Console.ReadLine();
-
         }
     }
 }
-git 
