2a76b617f453e663537f2c99ecf220ff48403dfa
WorldArtGames <wag.head.office@gmail.com>
2019-02-26 18:14:58 +0100

    5.3. Cikličke petlje

---
+++ ConsoleApp1/5.3.1.3.knjiga/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_3._1._3.knjiga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tablica množenja");
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write("{0}\t", (i * j).ToString());
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        //Ispis svih brojeva 0,1,2...99
+        Console.Writeline
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("{0}", i);
+            }
+        }
+    }
+}
