1638afc047830a0a16752de999fa737d07c025c4
WorldArtGames <wag.head.office@gmail.com>
2019-02-26 17:03:37 +0100

    zadaci dalje u nizu 5.2

---
+++ ConsoleApp1/ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a >= 100 && a <= 200)
+            {
+                Console.WriteLine("Broj jest u intervalu [100, 200].");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE u intervalu [100, 200]!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite jedan cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 5 == 0 || a % 3 == 0)
+            {
+                Console.WriteLine("Broj je dobar.");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE dobar!");
+            }
+            Console.ReadKey();
+        }
+       
+    }
+}
