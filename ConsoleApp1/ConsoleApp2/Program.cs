52a83eb16ce44abc574d02edfcd665edf9c5d6da
WorldArtGames <wag.head.office@gmail.com>
2019-02-26 16:38:02 +0100

    zadaci 5.2. ...

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Zadatak_5_2_3_Velik_Broj
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a >= 100)
+            {
+                Console.WriteLine("Broj je velik.");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE velik.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
