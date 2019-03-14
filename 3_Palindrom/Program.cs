e04e4dc2cc530701dbdf24f02d248b72680c8fb0
WorldArtGames <47826319+WorldArtGames@users.noreply.github.com>
2019-03-14 15:38:16 +0100

    Add files via upload

---
+++ 3_Palindrom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite riječ : ");
+            string text = Console.ReadLine();
+            int len = text.Length;
+            bool flag = true;
+
+            //provjera palindroma
+            for (int i = 0; i < len / 2; i++)
+            {
+                if (text[i] != text[len - (i + 1)])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            //test palindroma
+            if (flag)
+            {
+                Console.WriteLine("{0} je palindrom", text);
+            }
+            else
+            {
+                Console.WriteLine("{0} nije palindrom", text);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9.1.6_Palindrom_DZ/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1._6_Palindrom_DZ
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            // Rješenje br.1
+
+            Console.WriteLine("Unesite tekst");
+            string tekst = Console.ReadLine();
+
+            tekst = tekst.Replace(" ", "");
+
+            string obrnuto = "";
+            for (int i = tekst.Length - 1; i >= 0; i--)
+            {
+                obrnuto += tekst[i];
+            }
+            if (tekst == obrnuto)
+            {
+                Console.WriteLine("Unešeni tekst je palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Tekst nije palindrom");
+            }
+
+            // Rješenje br.2
+            string s, revs = "";
+            Console.WriteLine("Unesite rečenicu");
+            s = Console.ReadLine();
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                revs += s[i].ToString();
+            }
+            if (revs == s)
+            {
+                Console.WriteLine("Rečenica je PALINDROM", s, revs);
+            }
+            else
+            {
+                Console.WriteLine("Rečenica NIJE PALINDROM", s, revs);
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
